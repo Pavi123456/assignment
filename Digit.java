7dbc2d1baea2c7612b4199e27b44d7c5ac71ae13
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-07-27 09:51:00 +0530

    Create Digit.java

---
+++ Digit.java
@@ -0,0 +1,11 @@
+import java .util.Scanner.*;
+class Digit{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+int n=sc.nextInt();
+    for (i=0; i<n; i++) {
+        System.out.println("hello");
+    }
+    return 0;
+}
