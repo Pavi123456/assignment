bd052f31ecba0c5e47684525c3d95de1c18ce37d
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-08-04 10:27:50 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P3 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println(" enter the two numbers");
+	int a=sc.nextInt();
+	int b= sc.nextInt();
+	System.out.println("power of a number is"+Math.pow(a,b));
+}
+}
