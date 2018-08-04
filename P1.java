67c59dd04ac1ead1100c24b10bc4b04939615420
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-08-04 10:07:35 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class P1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the 3 numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>b && a>c)
+		{
+			System.out.println("a is big");
+			
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("b is big");
+		}
+		else
+		{
+			System.out.println("c is big");
+		}
+		}
+
+}
