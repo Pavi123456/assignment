8c2a5acd20a1f054666ab3b36b4cab2e00e3b2e2
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-08-30 09:56:55 +0530

    Add files via upload

---
+++ wordcount.java
@@ -0,0 +1,14 @@
+mport java.util.Scanner;
+
+public class Om {
+	
+public static void main(String[] args) {
+
+	System.out.println("enter the number");
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	String[] str1=str.split(" ");
+int length=str1.length;
+System.out.println(length);
+}
+}
