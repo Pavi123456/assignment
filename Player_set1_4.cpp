243cd8fc35c515b12b25843580f2efdf0b776cbb
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-01 17:42:39 +0530

    Add files via upload

---
+++ Player_set1_4.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+public class Player_set1_4
+int minimalSteps(string s, int n) 
+{ 
+  
+    int dp[n]; 
+  
+    
+    for (int i = 0; i < n; i++) 
+        dp[i] = INT_MAX; 
+  
+  
+    string s1 = "", s2 = ""; 
+      
+  
+    dp[0] = 1; 
+  
+    s1 += s[0]; 
+  
+    for (int i = 1; i < n; i++) { 
+        s1 += s[i]; 
+  
+       
+        s2 = s.substr(i + 1, i + 1); 
+  
+       
+        dp[i] = min(dp[i], dp[i - 1] + 1); 
+  
+        
+        if (s1 == s2) 
+            dp[i * 2 + 1] = min(dp[i] + 1, dp[i * 2 + 1]); 
+    } 
+  
+    return dp[n - 1]; 
+} 
+  
+
+int main() 
+{ 
+  
+    string s = "aaaaaaaa"; 
+    int n = s.length(); 
+  
+    
+    cout << minimalSteps(s, n); 
+  
+    return 0; 
+} 
