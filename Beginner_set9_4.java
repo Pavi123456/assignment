387126ddf0adfcddfc1c3f631f30fb7ecbd5deed
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-07 21:58:05 +0530

    Add files via upload

---
+++ Beginner_set9_4.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+import java.util.*;
+  public class Beginner_set9_4{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter a Charecter:");
+      char in=sc.next().charAt(0);
+      int Conv=(int)in;
+      System.out.println("ANS:"+Conv);
+      }
+} 
