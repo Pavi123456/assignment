5114c15a52d6d492957693f3b0883599f427fc7b
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-07 22:22:38 +0530

    Add files via upload

---
+++ Beginner_set10_6.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+  public class Beginner_set10_6{
+    public static void main(String args[]){
+      Scanner sc=new Scnner(System.in);
+      System.out.println("Enter A value:");
+      int a=sc.nextInt();
+      System.out.println("Enter D value:");
+      int d=sc.nextInt();
+      System.out.println("Enter N value:");
+      int n=sc.nextInt();
+      System.out.println("AP:"+((a*a)+(d*d)+(n)));
+      }
+} 
