b9fca675f0d9518193f811c22ddc713cf41ad4b8
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-07 21:57:28 +0530

    Add files via upload

---
+++ Beginner_set9_2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Beginner_set9_2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+       float a=s.nextFloat();
+       float b=s.nextFloat();
+       float c=a*b;
+       System.out.printf("%f",c);
+	// your code goes here
+	}
+}
