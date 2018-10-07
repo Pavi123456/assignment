7a568493fb2efc1e74bc2bd3a54532559121ba5e
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-07 22:23:40 +0530

    Add files via upload

---
+++ Beginner_set10_9.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+  public class Beginner_set10_9{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter A:");
+      int a=sc.nextInt();
+      System.out.println("Enter B:");
+      int b=sc.nextInt();
+      System.out.println("Enter C:");
+      int c=sc.nextInt();
+      System.out.println("(A*B)%C="+((a*b)%c));
+    }
+  }  
