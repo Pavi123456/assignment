290e8865fcd62718097afc116a1c9d08844a6e91
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-07 19:10:44 +0530

    Add files via upload

---
+++ Beginner_set3_2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class Beginner_set3_2
+{
+public static void main(String args[])
+(
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+max=a[0];
+for(int i=0;i<n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+System.out.print(max);
+}
+}
