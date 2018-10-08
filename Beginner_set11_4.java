87c8421fc10fd9b86efdb48b2a44471d4e80a60f
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-08 20:02:47 +0530

    Add files via upload

---
+++ Beginner_set11_4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class Beginner_set11_4{
+    public static void main(String aa[]){
+    double a,b,c;
+   Scanner s=new Scanner(System.in);
+    a=s.nextDouble();
+    b=s.nextDouble();
+    c=Math.pow(a, b);
+    System.out.print(c);
+  
+   
+           
+   }
+}
