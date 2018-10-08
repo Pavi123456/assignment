ebca2f39efc036c4591a56362f220cf765094695
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-08 20:02:23 +0530

    Add files via upload

---
+++ Beginner_set11_3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Beginner_set11_2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String s1[]=s.split("\\s");
+		for(int i=0;i<s1.length;i++)
+		{
+			char c=s1[i].charAt(0);
+			char t=Character.toUpperCase(c);
+			s1[i]=s1[i].replace(c,t);
+		}
+		for(int i=0;i<s1.length;i++)
+		{
+		if(i==s1.length-1)
+		System.out.print(s1[i]);
+		else
+		System.out.print(s1[i]+" ");
+		}
+	
+	}
+}
