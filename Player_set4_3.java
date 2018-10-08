1e0c0345d7c9683d2ee25dbbeac2b2ba0e5ff0a8
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-08 19:33:09 +0530

    Add files via upload

---
+++ Player_set4_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Player_set4_3 {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+	
+		if(a.contains(b))
+		
+		System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+
+}
