2264dcb084f819a88f0e989c8bd4c67dc37d6cc4
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-08 19:34:23 +0530

    Add files via upload

---
+++ Player_set4_6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Player_set4_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+float s;
+s=(float) (a*(3.14/180));
+System.out.println(s);
+	}
+
+}
