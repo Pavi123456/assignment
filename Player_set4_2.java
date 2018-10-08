600e18f3df8887858a1c65fdaafc738ac1545d4d
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-08 19:32:49 +0530

    Add files via upload

---
+++ Player_set4_2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Player_set4_1 {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int sz=in.nextInt();
+	int arr[]=new int[sz];
+	int cnt=0;
+	for(int i=0;i<sz;i++){
+		arr[i]=in.nextInt();
+	}
+	for(int j=0;j<sz-1;j++){
+		if(arr[j]<arr[j+1]){
+			cnt++;
+		}
+	}
+	if(cnt==arr.length-1){
+		System.out.println("yes");
+	}
+	else{
+		System.out.println("no");
+	}
+}
+}
