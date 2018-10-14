dbbe54f05ec1298d6e77d24c15901f95cf171198
varshini1 <31032592+varshini1@users.noreply.github.com>
2018-10-14 14:44:35 +0530

    Add files via upload

---
+++ Player_set6_8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.ArrayList;
+public class Player_set5_8{
+  public static void main(String aa[]){
+      int n;
+      int f=0;
+      Scanner s=new Scanner(System.in);
+      String g=s.nextLine();
+      String h=s.next();
+      String ll[]=g.split(" ");
+      ArrayList<String>a=new ArrayList<String>();
+      for(int i=0;i<ll.length;i++){
+          a.add(ll[i]);
+      }
+      int l=Collections.frequency(a,h);
+      System.out.println(l);
+
+    }
